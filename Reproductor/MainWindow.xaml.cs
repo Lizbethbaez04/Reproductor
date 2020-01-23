716608d62e12087f0cd3dc303f58e3b1ed4e6149
alumno <alumno@E122>
2020-01-23 12:50:16 -0700

    Slider

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace Reproductor
         AudioFileReader reader;
         //Comunicacion con la tarjeta de audio exclusivo para salidas
         WaveOut output;
+
+        bool dragging = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -50,6 +52,11 @@ namespace Reproductor
         private void Timer_Tick(object sender, EventArgs e)
         {
             lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+
+            if(!dragging)
+            {
+                sldTiempo.Value = reader.CurrentTime.TotalSeconds;
+            }            
         }
 
         void ListarDispositivosSalidas()
@@ -102,9 +109,11 @@ namespace Reproductor
                     lblTiempoFinal.Text = reader.TotalTime.ToString().Substring(0, 8);
                     lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
 
+                    sldTiempo.Maximum = reader.TotalTime.TotalSeconds;
+                    sldTiempo.Value = reader.CurrentTime.TotalSeconds;
+
                     timer.Start();
                 }
-
             }            
         }
 
@@ -136,5 +145,19 @@ namespace Reproductor
                 btnDetener.IsEnabled = true;
             }
         }
+
+        private void SldTiempo_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            dragging = true;
+        } 
+
+        private void SldTiempo_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            dragging = false;
+            if(reader != null && output !=null && output.PlaybackState != PlaybackState.Stopped)
+            {
+                reader.CurrentTime = TimeSpan.FromSeconds(sldTiempo.Value);
+            }
+        }        
     }
 }
